abcf2b2328ab4fcb43c78e79e3b7a45c88933346
Bitiquinho <consoni_2519@hotmail.com>
2020-02-01 20:13:40 -0300

    Revert last commit. Try reading and writing without delay

--- signal_io_epos.cpp
+++ signal_io_epos.cpp
@@ -201,13 +201,9 @@ bool Write( long int deviceID, unsigned int channel, double value )
   
   DeviceData* device = (DeviceData*) deviceID;
   
-  BOOL status = 0;
-  DWORD errorCode;
-  if( channel == 0 ) status = VCS_SetPositionMust( device->handle, device->nodeId, (long) value, &errorCode );
-  else if( channel == 1 ) status = VCS_SetVelocityMust( device->handle, device->nodeId, (long) value, &errorCode );
-  else if( channel == 2 ) status = VCS_SetCurrentMust( device->handle, device->nodeId, (short) value, &errorCode );
+  device->outputValues[ channel ] = value;
 
-  if( status == 0 ) 
+  if( device->writeStatus == 0 ) 
   {
     PrintError( device->writeErrorCode );
     return false;
@@ -271,9 +267,16 @@ static void AsyncTransfer( void )
       device->inputValues[ 2 ] = (double) sValue;
       
       if( device->readStatus == 0 ) VCS_ClearFault( device->handle, device->nodeId, &(device->readErrorCode) );
+      
+      iValue = (long) device->outputValues[ 0 ];
+      device->writeStatus = VCS_SetPositionMust( device->handle, device->nodeId, iValue, &(device->writeErrorCode) );
+      iValue = (long) device->outputValues[ 1 ];
+      device->writeStatus = VCS_SetVelocityMust( device->handle, device->nodeId, iValue, &(device->writeErrorCode) );
+      sValue = (short) device->outputValues[ 2 ];
+      device->writeStatus = VCS_SetCurrentMust( device->handle, device->nodeId, sValue, &(device->writeErrorCode) );
     }
     
-    std::this_thread::sleep_for( std::chrono::milliseconds( 5 ) );
+    //std::this_thread::sleep_for( std::chrono::milliseconds( 5 ) );
   }
   
   return;
